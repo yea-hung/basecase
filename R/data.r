8e9d9b07768e0c78e382bbfa2e92b01d873cfa10
yea-hung <7434605+yea-hung@users.noreply.github.com>
2025-03-13 10:37:41 -0700

    updated data

--- R/data.r
+++ R/data.r
@@ -8,7 +8,6 @@
 #'   \item{seqn}{a unique identifier}
 #'   \item{dmdborn4}{country of birth}
 #'   \item{lbxtc}{total cholesterol}
-#'   ...
 #' }
 #' @source <https://wwwn.cdc.gov/nchs/nhanes/continuousnhanes/default.aspx?Cycle=2021-2023>
 "nhanes"
