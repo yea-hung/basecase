bd631838e9212b72d050b11585d8998097cd37ac
yea-hung <7434605+yea-hung@users.noreply.github.com>
2025-05-05 09:32:50 -0700

    added an argument for getting a character output

--- R/base_when.r
+++ R/base_when.r
@@ -12,21 +12,23 @@
 #'    'Borderline high' = (nhanes$lbxtc>=200)&(nhanes$lbxtc<240),
 #'    'High' = (nhanes$lbxtc>=240)
 #' )
-base_when<-function(...){
+base_when<-function(...,as_factor=TRUE,string_for_na=''){
 
   # define the conditions
   conditions<-list(...)
 
   # define the new variable
-  new_variable<-rep('',length(conditions[[1]]))
+  new_variable<-rep(string_for_na,length(conditions[[1]]))
 
   # define the variable's groups
   for(position in 1:length(conditions)){
     new_variable[conditions[[position]]]<-names(conditions)[position]
   }
-
-  # convert the variable to a factor
-  new_variable<-factor(new_variable,levels=unique(names(conditions)))
+  
+  # convert the variable to a factor, if a factor is desired
+  if(as_factor){
+    new_variable<-factor(new_variable,levels=unique(names(conditions)))  
+  }
 
   # return the object
   new_variable
