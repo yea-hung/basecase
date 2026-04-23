b9c8e340997f1de33fcf9566471d4b02601c9c12
yea-hung <7434605+yea-hung@users.noreply.github.com>
2026-04-22 18:39:32 -0700

    minor updates

--- R/base_match.r
+++ R/base_match.r
@@ -1,14 +1,22 @@
-#' `base_match()`
+#' A base-R approxmation of `case_match()`
 #'
 #' A base-R approximation of *dplyr*'s `case_match()`. Unlike `case_match()`, `base_when()` returns a factor. The levels will be ordered according to the order included in `codebook`.
 #' @param original_variable the original variable
-#' @param codebook the codebook, specified as a named vector, with each element in `'label'=level` format, with the levels listed in the desired order
+#' @param ... the codebook, specified as a named vector, with each element in `'label'=level` format, with the levels listed in the desired order
+#' @param as_factor logical, controlling whether the function should return a factor
+#' @param string_for_na string value that will be converted to NA
 #'
 #' @returns a factor
 #' @export
 #'
 #' @examples
-#' nhanes$country<-base_match(nhanes$dmdborn4,'USA'=1,'Other'=2)
+#' # load data
+#' data(nhanes)
+#' 
+#' # define country
+#' nhanes<-nhanes |> transform(
+#'   country=base_match(nhanes$dmdborn4,'USA'=1,'Other'=2)
+#' )
 base_match<-function(original_variable,...,as_factor=TRUE,string_for_na=''){
 
   # define the codebook
--- R/base_when.r
+++ R/base_when.r
@@ -1,17 +1,27 @@
-#' `base_when()`
+#' A base-R approxmation of `case_when()`
 #'
 #' A base-R approximation of *dplyr*'s `case_when()`. Unlike `case_when()`, `base_when()` returns a factor. The levels will be ordered according to the order included in `conditions`.
-#' @param conditions conditions for defining the replacement values, specified as a named list, with each element in `'label'=logical_vector` format, with the levels listed in the desired order
+#' 
+#' @param ... conditions for defining the replacement values, specified as a named list, with each element in `'label'=logical_vector` format, with the levels listed in the desired order
+#' @param as_factor logical, controlling whether the function should return a factor
+#' @param string_for_na string value that will be converted to NA
 #'
 #' @returns a factor
 #' @export
 #'
 #' @examples
-#' nhanes$cholesterol<-base_when(
-#'    'Desirable' = (nhanes$lbxtc<200),
-#'    'Borderline high' = (nhanes$lbxtc>=200)&(nhanes$lbxtc<240),
-#'    'High' = (nhanes$lbxtc>=240)
-#' )
+#' # load data
+#' data(nhanes)
+#' 
+#' # define cholesterol
+#' nhanes<-nhanes |>
+#'   transform(
+#'     cholesterol=base_when(
+#'       'Desirable' = (lbxtc<200),
+#'       'Borderline high' = (lbxtc>=200)&(lbxtc<240),
+#'       'High' = (lbxtc>=240)
+#'     )
+#'   )
 base_when<-function(...,as_factor=TRUE,string_for_na=''){
 
   # define the conditions
